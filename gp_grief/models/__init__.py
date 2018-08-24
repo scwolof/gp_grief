d4f53dd1dcad12aed490d57f9eebc5b0c3d4b486
scwolof <scw.olofsson@gmail.com>
2018-08-24 16:22:42 +0100

    import GPRegressionModel

--- gp_grief/models/__init__.py
+++ gp_grief/models/__init__.py
@@ -1,5 +1,6 @@
 
 from .basemodel import BaseModel
+from .gpr_model import GPRegressionModel
 from .gp_grief_model import GPGriefModel
 from .gp_web_model import GPwebModel
 from .gp_web_transformed_model import GPwebTransformedModel
