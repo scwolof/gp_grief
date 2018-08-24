ec6918c746e5261ad5e549e43c9fcd8d43ba68aa
scwolof <scw.olofsson@gmail.com>
2018-08-24 13:11:19 +0100

    Updated __version__ file

--- gp_grief/__version__.py
+++ gp_grief/__version__.py
@@ -1,4 +1,3 @@
+# pragma: no cover
 __version__      = '1.0'
 __author__       = 'Trefor Evans and Simon Olofsson'
-#__author_email__ = 'simon.olofsson15@imperial.ac.uk'
-#__license__      = 'MIT License'
