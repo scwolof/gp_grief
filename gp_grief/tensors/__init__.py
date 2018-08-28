19d9b6d3d722a62e8d03f6e4f5635da834e34beb
scwolof <scw.olofsson@gmail.com>
2018-08-28 12:15:49 +0100

    Fixed import order

--- gp_grief/tensors/__init__.py
+++ gp_grief/tensors/__init__.py
@@ -1,9 +1,9 @@
 
-from .tensors import TensorProduct, TensorSum, Array, expand_SKC
-
 from .kron_matrix import KronMatrix
 from .selection_matrix import SelectionMatrix, SelectionMatrixSparse
 from .block_matrix import BlockMatrix
+
+from .tensors import TensorProduct, TensorSum, Array, expand_SKC
 
 from .khatri_rao_matrix import KhatriRaoMatrix, RowColKhatriRaoMatrix,\
 								RowColKhatriRaoMatrixTransposed
